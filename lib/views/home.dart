9a1cf6e8fdc3d5505a7068d649ac5425aa7f77d1
c3dr1c <4cedrictapsoba4@gmail.com>
2022-08-20 19:44:44 +0000

    not important commit - acceuil implementation 0.1

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -8,8 +8,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _selectedIndex = 0;
+  PageController pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Text('text'));
+    return Scaffold(
+      appBar: AppBar(
+        title:  Container(child: const Image(image: AssetImage("assets/images/logo.png",),width: 100,)),
+      centerTitle: true,
+      leading: IconButton(onPressed: (){}, icon: const Icon(Icons.menu, color: Colors.green,)),
+      actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.notifications, color: Colors.yellowAccent)), IconButton(onPressed: (){}, icon: const Icon(Icons.account_circle_rounded, color: Colors.purple,))],
+      backgroundColor: Colors.white,),
+      body: PageView(
+        controller: pageController,
+      
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon:Icon(Icons.home),label: "Acceuil"),
+        BottomNavigationBarItem(icon:Icon(Icons.message),label: "Echange"),
+        BottomNavigationBarItem(icon:Icon(Icons.notifications),label: "Notification"),],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green,),
+    );
   }
 }
