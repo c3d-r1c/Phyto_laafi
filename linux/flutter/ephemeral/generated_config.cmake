37aeb093399a4ce8de228352c6d81710cf6b57d1
c3dr1c <4cedrictapsoba4@gmail.com>
2022-08-31 19:09:35 +0000

    initial commit in main

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/c3dr1c/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/c3dr1c/Bureau/Phyto_laafi" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/c3dr1c/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/c3dr1c/Bureau/Phyto_laafi"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/c3dr1c/Bureau/Phyto_laafi/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/c3dr1c/Bureau/Phyto_laafi/lib/main.dart"
+)
